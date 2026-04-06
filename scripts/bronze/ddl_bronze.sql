a64d2e8ae980314053dbfca339e8d498991da5c0
Mehiko26 <okonkwoemmanuel007@gmail.com>
2026-04-06 22:14:29 +0100

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,7 @@
+/*
+===================================================
+DDL Script: Create Bronze Tables
+===================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables if hey already exist 
+Run this script to re-define the DDL structure of the 'bronze' tables
